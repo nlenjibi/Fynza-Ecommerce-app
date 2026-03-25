4ea543ec46a768b65d1cecd93762f26971b7d802
Nlenjibi Timothy <nlenjibitimo@gmail.com>
2026-03-25 14:01:36 +0000

    refactor(faq): extract FAQ input/DTO classes to packages

--- backend/src/main/java/ecommerce/graphql/resolver/FAQResolver.java
+++ backend/src/main/java/ecommerce/graphql/resolver/FAQResolver.java
@@ -1,7 +1,12 @@
 package ecommerce.graphql.resolver;
 
-import ecommerce.common.enums.FAQCategory;
 import ecommerce.common.response.PaginatedResponse;
+import ecommerce.graphql.dto.FAQCategoryCount;
+import ecommerce.graphql.dto.FAQConnection;
+import ecommerce.graphql.input.FAQFilterInput;
+import ecommerce.graphql.input.FAQInput;
+import ecommerce.graphql.input.PageInput;
+import ecommerce.graphql.input.SortDirection;
 import ecommerce.modules.faq.dto.CreateFAQRequest;
 import ecommerce.modules.faq.dto.FAQResponse;
 import ecommerce.modules.faq.dto.UpdateFAQRequest;
@@ -145,74 +150,9 @@ public class FAQResolver {
         if (input == null) {
             return PageRequest.of(0, 20, Sort.by(Sort.Direction.ASC, "displayOrder"));
         }
-        Sort.Direction direction = "DESC".equalsIgnoreCase(input.getDirection()) 
+        Sort.Direction direction = input.getDirection() == SortDirection.DESC
             ? Sort.Direction.DESC : Sort.Direction.ASC;
         String sortBy = input.getSortBy() != null ? input.getSortBy() : "displayOrder";
         return PageRequest.of(input.getPage(), input.getSize(), Sort.by(direction, sortBy));
-    }
-
-    public static class PageInput {
-        private int page = 0;
-        private int size = 20;
-        private String sortBy;
-        private String direction;
-
-        public int getPage() { return page; }
-        public void setPage(int page) { this.page = page; }
-        public int getSize() { return size; }
-        public void setSize(int size) { this.size = size; }
-        public String getSortBy() { return sortBy; }
-        public void setSortBy(String sortBy) { this.sortBy = sortBy; }
-        public String getDirection() { return direction; }
-        public void setDirection(String direction) { this.direction = direction; }
-    }
-
-    public static class FAQFilterInput {
-        private FAQCategory category;
-        private Boolean isActive;
-        private Boolean isPublic;
-        private String search;
-
-        public FAQCategory getCategory() { return category; }
-        public void setCategory(FAQCategory category) { this.category = category; }
-        public Boolean getIsActive() { return isActive; }
-        public void setIsActive(Boolean isActive) { this.isActive = isActive; }
-        public Boolean getIsPublic() { return isPublic; }
-        public void setIsPublic(Boolean isPublic) { this.isPublic = isPublic; }
-        public String getSearch() { return search; }
-        public void setSearch(String search) { this.search = search; }
-    }
-
-    public static class FAQInput {
-        private String question;
-        private String answer;
-        private FAQCategory category;
-        private Integer order;
-        private Boolean isPublic;
-
-        public String getQuestion() { return question; }
-        public void setQuestion(String question) { this.question = question; }
-        public String getAnswer() { return answer; }
-        public void setAnswer(String answer) { this.answer = answer; }
-        public FAQCategory getCategory() { return category; }
-        public void setCategory(FAQCategory category) { this.category = category; }
-        public Integer getOrder() { return order; }
-        public void setOrder(Integer order) { this.order = order; }
-        public Boolean getIsPublic() { return isPublic; }
-        public void setIsPublic(Boolean isPublic) { this.isPublic = isPublic; }
-    }
-
-    @lombok.Data
-    @lombok.Builder
-    public static class FAQConnection {
-        private List<FAQResponse> content;
-        private PaginatedResponse<FAQResponse> pageInfo;
-    }
-
-    @lombok.Data
-    @lombok.Builder
-    public static class FAQCategoryCount {
-        private FAQCategory category;
-        private Integer count;
     }
 }
